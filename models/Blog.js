ab667a556cce695f414bf27681e567a51a2a7b8e
Ankit Kumar <71378462+Ankit62042@users.noreply.github.com>
2024-02-19 00:22:37 +0530

    Update Blog.js

--- models/Blog.js
+++ models/Blog.js
@@ -5,10 +5,10 @@ const blogSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   country: { type: String, required: true }, 
-  author: { type: String, required: true },
-  tags: { type: String, required: true}, 
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now } 
+  //author: { type: String, required: true },
+  //tags: { type: String, required: true}, 
+  //createdAt: { type: Date, default: Date.now },
+  //updatedAt: { type: Date, default: Date.now } 
 }, { timestamps: true }); 
 
 module.exports = mongoose.model('Blog', blogSchema);
