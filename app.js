0b988f4037414a68d842339c419076756398897d
Ankit Kumar <71378462+Ankit62042@users.noreply.github.com>
2024-02-19 02:06:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const path = require('path');
 // Routes
 const eventsRoutes = require('./routes/events'); 
 const blogsRoutes = require('./routes/blogs'); 
